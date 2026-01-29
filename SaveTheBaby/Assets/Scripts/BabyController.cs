81436e4c9d60b5a88dbc0f117336fae121a09e1e
rlawson04 <114245674+rlawson04@users.noreply.github.com>
2026-01-29 16:28:59 -0500

    Added baby death sound

--- SaveTheBaby/Assets/Scripts/BabyController.cs
+++ SaveTheBaby/Assets/Scripts/BabyController.cs
@@ -21,6 +21,7 @@ public class BabyController : MonoBehaviour
     private bool hitSomething = false;
     private GameObject lastGoalHit = null;
     private SpriteRenderer sr;
+    bool babyAlive = true;
 
     [SerializeField] private float crawlSpeed = 1;
     [SerializeField] private int crawlDir = 1;
@@ -113,10 +114,17 @@ public class BabyController : MonoBehaviour
     //destroys the baby
     private void KillBaby()
     {
+        if(!babyAlive)
+        {
+            return;
+        }
+        babyAlive = false;
         Debug.Log("Baby Died!");
         crawlDir = 0;
         IEnumerator babyRoutine = ReloadSceneAfterDelay(5);
         StartCoroutine(babyRoutine);
+        GameObject.Find("AudioManager").GetComponent<AudioSource>().Play();
+
     }
 
 
